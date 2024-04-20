bdb3101785973d1532fc41e7249edc130cd0d35f
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-04-21 00:22:11 +0530

    Fixed Moving Tape For Projects

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
+      from: "Contact from <onboarding@resend.dev>",
       to: "prashant.sagar.shakya@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
