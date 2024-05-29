00e4ac71ac33f688e24e6ff2d3173c9b41c86dd1
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-05-29 16:21:08 +0530

    linkedin updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -32,7 +32,7 @@ export default function Intro() {
                         }}
                     >
                         <Image
-                            src= {profile}
+                            src={profile}
                             alt="Prashant portrait"
                             width="192"
                             height="192"
@@ -106,7 +106,7 @@ export default function Intro() {
 
                 <a
                     className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                    href="https://linkedin.com"
+                    href="https://www.linkedin.com/in/prashant162003/"
                     target="_blank"
                 >
                     <BsLinkedin />
