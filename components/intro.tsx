d2201588346379f00e2ae5c1785942cfc132ee80
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-05-29 16:05:22 +0530

    Profile picture imported and updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,6 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import profile from "@/public/profile.jpg";
 
 export default function Intro() {
     const { ref } = useSectionInView("Home", 0.5);
@@ -31,7 +32,7 @@ export default function Intro() {
                         }}
                     >
                         <Image
-                            src="https://avatars.githubusercontent.com/u/166375759?v=4"
+                            src= {profile}
                             alt="Prashant portrait"
                             width="192"
                             height="192"
